db9ab7c836345d7ac84676cc2101157365adbb5d
lioness3 <jcnailtech3@yahoo.com>
2020-04-20 12:11:37 -0700

    add green

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -14,7 +14,7 @@ function MainPage() {
   return (
     <div className="mainPage">
       <Menu/> 
-      {/* <img src={leaves} className='stillLeaves' alt='stillLeaves'/> */}
+      <img src={leaves} className='stillLeaves' alt='stillLeaves'/>
               <img src={leaves} className='leaves' alt='leaves'/>
               <img src={leaf} className='leaf' alt='leaf'/>
               <img src={lion} className='lion' alt='lion'/>
