bf3f321c6968357ab294572cae0fc3eddcebecc1
lioness3 <jcnailtech3@yahoo.com>
2020-04-30 15:12:46 -0700

    collapse

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -17,19 +17,19 @@ function Menu() {
       </div>
       
       <div className="tooltip menuCol">
-        <a href='https://github.com/lioness3' className='githubLink'>GitHub</a>
+        <a href='https://github.com/lioness3' target = "_blank" className='githubLink'>GitHub</a>
         <span className="tooltiptext">
           Github Link</span>
       </div>
 
-      <div className='tooltip menuCol'>
+      <div className='tooltip menuCol'  >
       {/* <div className='contactDropDown'>
         <span className='contact'>Contact </span>
         <div className= 'contactDetails'>
           <div><Mailto email='joann333carter@gmail.com'/> </div><div><Mailto tel='603-566-5610'/></div>
         </div>
       </div> */}
-       <span className='contact'>Contact </span>
+       <span className='contact' >Contact </span>
         <span className='tooltiptext'>Contact Me Directly</span>
       </div>
      
--- src/pages/ColorfulPage.js
+++ src/pages/ColorfulPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import Menu from '../components/Menu';
 
@@ -16,8 +16,42 @@ import Niagra from '../components/images/Niagra.jpeg'
 import cat from '../components/images/cat.jpeg'
 import craft from '../components/images/craft.jpeg'
 function ColorfulPage() {
-
-
+const [displayMentionables, setDisplayMentionables] = useState('infoHidden')
+const [displayFavorites, setDisplayFavorites] = useState('infoHidden')
+const [displayAdventures, setDisplayAdventures]= useState('infoHidden')
+const [displayHobbies, setDisplayHobbies] = useState('infoHidden')
+const showMentionablesInfo = ()=>{
+  if (displayMentionables === 'infoHidden'){
+    setDisplayMentionables('info')
+  }else{
+    setDisplayMentionables('infoHidden')
+  }
+  
+}
+const showFavoritesInfo = ()=>{
+  if (displayFavorites === 'infoHidden'){
+    setDisplayFavorites('info')
+  }else{
+    setDisplayFavorites('infoHidden')
+  }
+  
+}
+const showAdventuresInfo = ()=>{
+  if (displayAdventures === 'infoHidden'){
+    setDisplayAdventures('stories')
+  }else{
+    setDisplayAdventures('infoHidden')
+  }
+  
+}
+const showHobbiesInfo = ()=>{
+  if (displayHobbies === 'infoHidden'){
+    setDisplayHobbies('stories')
+  }else{
+    setDisplayHobbies('infoHidden')
+  }
+  
+}
   return (
     <div className="colorfulPage">
  
@@ -28,66 +62,76 @@ function ColorfulPage() {
       <div className='zoomContainer'>
 
         <div className='section' >
-        <p className='heading'>RECENT ADVENTURE</p>
+        <p className='heading'>ADVENTURES<span className='carrot' onClick={()=>{showAdventuresInfo()}}> ...</span></p>
+        <div className = {displayAdventures}>
           <p className='caption'>I recently drove across the country with my co pilot, Shadow. </p>
           <img src={coPiolit} className='coPiolit' alt='cat image'/>
-          <p className='caption'>First, We drove from New Hampshire to Niagra Falls</p>
+          <p className='caption'>We explored Niagra Falls</p>
           <img src={Niagra} className='niagra' alt='image'/>
           <p className='caption'> Our final destination was Washington, where we currently reside.</p>
           <img src={cat} className='cat' alt='cat'/>
     
           <img src={shadow} className='shadow' alt='cat'/>
-  
+  </div>
         </div>
 
        
 
         <div className='section'>
-          <p className='heading'>HOBBIES</p>
+          <p className='heading'>HOBBIES<span className='carrot' onClick={()=>{showHobbiesInfo()}}> ...</span></p>
+          <div className = {displayHobbies}>
           <p className='caption'>I feel grounded when im in nature.</p> 
           <img src={waterfallAdventure} className='waterfallAdventure' alt='image'/>
           <p className='caption'>I enjoy creating crafts out of recycled material. </p>
           <img src={craft} className='craft' alt='craft image' /> 
+          </div>
         </div>
       </div>
-       <div className=' skills'>
-          <p className='skillsCaption'>SKILLS</p>
-          <div className='skillsList'>
-            <span>RUBY </span>
-            <span>RAILS</span>
-            <span>IONIC</span>
-            <span>REACT</span>
-            <span>REACT NATIVE</span>
-            <span>NPM</span>
-            <span>PAIR PROGRAMMING</span>
+      <div className='zoomContainer2'>
+       <div className='section2 '>
+          <p className=' heading2 '>MENTIONABLES<span className='carrot' onClick={()=>{showMentionablesInfo()}}> ...</span></p>
+          <div className = {displayMentionables}>
+            <p>React</p>
+            <p>React Native</p>
+            <p>Ruby </p>
+            <p>Rails</p>
+            <p>Ionic</p>
+            <p>Pair Programming</p>
+            <p>Associates in Science </p>
+            <p>Web and Mobile Developer</p>
+            <p>Ham Radio License</p>
+            <p>Licensed Manicurist</p>
+            <p>Sig Sauer Certifications</p>
+            <p>Barbizon Modeling Academy</p>
           </div>
          
         </div> 
-         <div className=' clip'>
-          <p className='clipCaption'>Some of my favorite things</p>
-          <div className='favorites'>
-            <span>PIZZA</span>
-            <span>ICE CREAM</span>
-            <span>PUZZLES</span>
-            <span>TACOS</span>
-            <span>FAMILY</span>
-            <span>RACE CARS</span>
-            <span>CRAFTS</span>
-        
+         <div className=' section2 '>
+          <p className=' heading2 '>FAVORITES<span  className='carrot' onClick={()=>{showFavoritesInfo()}}> ...</span></p>
+          <div className={displayFavorites}>
+
+              <p className='pizza'>Pizza <span className='pizzaEmoji'>🍕</span></p>
+              <p className='iceCream'>Ice Cream <span className='iceCreamEmoji'>🍦</span></p>
+              <p className='puzzles'>Puzzles <span className='puzzlesEmoji'>🧩</span></p>
+              <p className='tacos'>Tacos <span className='tacosEmoji'>🌮</span> </p>
+              <p className='family'>Family <span className='familyEmoji'>👨‍👩‍👧</span></p>
+              <p className='raceCar'>Race Cars <span className='raceCarEmoji'>🏎</span></p>
+              <p className='crafts'>Crafts <span className='craftsEmoji'>🧶</span></p>
+          
+         
+
+             
+  
+            
+         
+  
+             
+          
           </div>
 
         </div>  
-        <div className=' accomplishments'>
-          <p className='accomplishmentsCaption'>ACCOMPLISHMENTS</p>
-          <div className='accomplishmentsList'>
-            <span>ASSOCIATES IN SCIENCE </span>
-            <span>WEB AND MOBILE DEVELOPER</span>
-            <span>HAM RADIO LICENSE</span>
-            <span>LICENSED NAIL TECHNICIAN</span>
-            <span>SIG SAUER CERTIFICATIONS</span>
-            <span>BARBIZON MODELING ACADEMY</span>
-          </div>
-        </div>  
+      
+        </div> 
      </div>
 
   );
