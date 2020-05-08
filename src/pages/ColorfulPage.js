78fbce80db3b31efb1ee76d9b6b8f87f0e437ba3
lioness3 <jcnailtech3@yahoo.com>
2020-05-07 18:36:27 -0700

    change layout

--- src/pages/ColorfulPage.js
+++ src/pages/ColorfulPage.js
@@ -19,6 +19,7 @@ const [displayMentionables, setDisplayMentionables] = useState('infoHidden')
 const [displayFavorites, setDisplayFavorites] = useState('infoHidden')
 const [displayAdventures, setDisplayAdventures]= useState('infoHidden')
 const [displayHobbies, setDisplayHobbies] = useState('infoHidden')
+const [display, setDisplay]= useState(null)
 const showMentionablesInfo = ()=>{
   if (displayMentionables === 'infoHidden'){
     setDisplayMentionables('info')
@@ -51,78 +52,132 @@ const showHobbiesInfo = ()=>{
   }
   
 }
+
+const fav=(
+  <div className='content'>
+    <div  onClick={()=>{
+      handleDisplay(null)
+    }}> <p className='closeView'>x</p>
+    <p className='banner'>FAVORITES</p></div>
+   
+    <p className='pizza'>Pizza <span className='pizzaEmoji'>🍕</span></p>
+    <p className='iceCream'>Ice Cream <span className='iceCreamEmoji'>🍦</span></p>
+    <p className='puzzles'>Puzzles <span className='puzzlesEmoji'>🧩</span></p>
+    <p className='tacos'>Tacos <span className='tacosEmoji'>🌮</span> </p>
+    <p className='family'>Family <span className='familyEmoji'>👨‍👩‍👧</span></p>
+    <p className='raceCar'>Race Cars <span className='raceCarEmoji'>🏎</span></p>
+    <p className='crafts'>Crafts <span className='craftsEmoji'>🧶</span></p>
+  </div>)
+const interests = (
+  <div className='content'>
+    <div className='closeView' onClick={()=>{
+      handleDisplay(null)
+    }}>x</div>
+    <p className='banner'>INTERESTS</p>
+    <p>Front End Developer</p>
+    <p>UI Design</p>
+    <p>UX Design</p>
+  </div>
+)
+const languages = (
+  <div className='content'>
+    <div className='closeView' onClick={()=>{
+      handleDisplay(null)
+    }}>x</div>
+    <p className='banner'>LANGUAGES</p>
+    <p>React</p>
+    <p>React Native</p>
+    <p>Ruby </p>
+    <p>Rails</p>
+    <p>Ionic</p>
+  </div>
+)
+const skills = (
+  <div className='content'>
+    <div className='closeView' onClick={()=>{
+      handleDisplay(null)
+    }}>x</div>
+    <p className='banner'>SKILLS</p>
+    <p>Pair Programming</p>
+    <p>User Experiance </p>
+    <p>Web and Mobile Developer</p>
+    <p>Customer Service</p>
+  </div>
+)
+const funFacts = (
+  <div className='content'>
+    <div className='closeView' onClick={()=>{
+      handleDisplay(null)
+    }}>x</div>
+    <p className='banner'>FUN FACTS</p>
+    <p>Ham Radio License</p>
+    <p>Licensed Manicurist</p>
+    <p>Sig Sauer Certifications</p>
+    <p>Barbizon Modeling Academy</p>
+  </div>
+)
+const adventures = (
+  <div className='detailedContent content' >  
+    <div className='closeView' onClick={()=>{
+      handleDisplay(null)
+    }}>x</div>
+    <p className='banner'>ADVENTURES</p>
+    <p className='caption'>I recently drove across the country with my co pilot, Shadow. </p>
+    <img src={coPiolit} className='coPiolit' alt='cat image'/>
+    <p className='caption'>We explored Niagra Falls</p>
+    <img src={Niagra} className='niagra' alt='image'/>
+    <p className='caption'> Our final destination was Washington, where we currently reside.</p>
+    <img src={cat} className='cat' alt='cat'/>
+    <img src={shadow} className='shadow' alt='cat'/>
+  </div>
+)
+const crafts =(
+
+  <div className='detailedContent content'>
+    <div className='closeView' onClick={()=>{
+      handleDisplay(null)
+    }}>x</div>
+    <p className='banner'>HOBBIES</p>
+    <p className='caption'>I feel grounded when im in nature.</p> 
+    <img src={waterfallAdventure} className='waterfallAdventure' alt='image'/>
+    <p className='caption'>I enjoy creating crafts out of recycled material. </p>
+    <img src={craft} className='craft' alt='craft image' /> 
+  </div>
+)
+const handleDisplay=(content)=>{
+setDisplay(content)
+}
   return (
-    <div className="colorfulPage">
+    <div className="colorfulPage" >
  
         <Link to='/' className='homeLink'>Joann Carter</Link>
-
-  
-  
-      <div className='zoomContainer'>
-
-        <div className='section' >
-        <p className='heading'>ADVENTURES<span className='carrot' onClick={()=>{showAdventuresInfo()}}> ...</span></p>
-        <div className = {displayAdventures}>
-          <p className='caption'>I recently drove across the country with my co pilot, Shadow. </p>
-          <img src={coPiolit} className='coPiolit' alt='cat image'/>
-          <p className='caption'>We explored Niagra Falls</p>
-          <img src={Niagra} className='niagra' alt='image'/>
-          <p className='caption'> Our final destination was Washington, where we currently reside.</p>
-          <img src={cat} className='cat' alt='cat'/>
-    
-          <img src={shadow} className='shadow' alt='cat'/>
-  </div>
-        </div>
-
-       
-
-        <div className='section'>
-          <p className='heading'>HOBBIES<span className='carrot' onClick={()=>{showHobbiesInfo()}}> ...</span></p>
-          <div className = {displayHobbies}>
-          <p className='caption'>I feel grounded when im in nature.</p> 
-          <img src={waterfallAdventure} className='waterfallAdventure' alt='image'/>
-          <p className='caption'>I enjoy creating crafts out of recycled material. </p>
-          <img src={craft} className='craft' alt='craft image' /> 
-          </div>
-        </div>
-      </div>
-      <div className='zoomContainer2'>
-       <div className='section2 '>
-          <p className=' heading2 '>MENTIONABLES<span className='carrot' onClick={()=>{showMentionablesInfo()}}> ...</span></p>
-          <div className = {displayMentionables}>
-            <p>React</p>
-            <p>React Native</p>
-            <p>Ruby </p>
-            <p>Rails</p>
-            <p>Ionic</p>
-            <p>Pair Programming</p>
-            <p>Associates in Science </p>
-            <p>Web and Mobile Developer</p>
-            <p>Ham Radio License</p>
-            <p>Licensed Manicurist</p>
-            <p>Sig Sauer Certifications</p>
-            <p>Barbizon Modeling Academy</p>
-          </div>
+        <div className='titleCol' >
+          <div className=' title' onClick={()=>{handleDisplay(languages)}}>
+            <p>LANGUAGES</p>
+          </div> 
+          <div className=' title ' onClick={()=>{handleDisplay(funFacts)}}>
+            <p>FUN FACTS</p>
+          </div> 
+          <div className='title' onClick={()=>{handleDisplay(skills)}}>
+            <p>SKILLS</p>
+          </div> 
+          <div className='title' onClick={()=>{handleDisplay(interests)}}>
+            <p>INTERESTS</p>
+          </div> 
+          <div className='title' onClick={()=>{handleDisplay(fav)}}>
+            <p>FAVORITES</p>
+          </div>  
+          <div className='title' onClick={()=>{handleDisplay(adventures)}}>
+            <p>ADVENTURES</p>
+          </div>  
+          <div className='title' onClick={()=>{handleDisplay(crafts)}}>
+            <p>CRAFTS</p>
+          </div> 
          
-        </div> 
-         <div className=' section2 '>
-          <p className=' heading2 '>FAVORITES<span  className='carrot' onClick={()=>{showFavoritesInfo()}}> ...</span></p>
-          <div className={displayFavorites}>
-
-              <p className='pizza'>Pizza <span className='pizzaEmoji'>🍕</span></p>
-              <p className='iceCream'>Ice Cream <span className='iceCreamEmoji'>🍦</span></p>
-              <p className='puzzles'>Puzzles <span className='puzzlesEmoji'>🧩</span></p>
-              <p className='tacos'>Tacos <span className='tacosEmoji'>🌮</span> </p>
-              <p className='family'>Family <span className='familyEmoji'>👨‍👩‍👧</span></p>
-              <p className='raceCar'>Race Cars <span className='raceCarEmoji'>🏎</span></p>
-              <p className='crafts'>Crafts <span className='craftsEmoji'>🧶</span></p>
-          
-          
-          </div>
-
-        </div>  
-      
-        </div> 
+        </div>
+        <div className='changeableContent'>
+          {display}
+        </div>
      </div>
 
   );
