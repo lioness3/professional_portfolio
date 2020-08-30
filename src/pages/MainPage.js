a30371605cf7a7718954ced8a155ff34e7ba98fd
lioness3 <jcnailtech3@yahoo.com>
2020-08-30 07:45:35 -0400

    create contact me button

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -9,7 +9,14 @@ import star from '../components/images/star.png'
 
 
 function MainPage() {
-const [contactInfo, setContactInfo]= useState(false)
+const [contact, setContactInfo]= useState(null)
+const contactMe = (
+  <div>
+    <MdEmail/>
+   <p>Contact Me</p>
+   </div>
+)
+const [details, setDetails] = useState(contactMe)
   const [available, setAvailability] = useState('available')
   const handleAppStore = ()=>{
     console.log('clicked');
@@ -20,34 +27,47 @@ const [contactInfo, setContactInfo]= useState(false)
       setAvailability('available')
     }
   }
-  const handleContactInfo = ()=>{
-   if (contactInfo){
-      setContactInfo(true)
-    }else {
-      setContactInfo(false)
-    }
-  }
+
  
+
+ const contactInfo = (
+ <div className='contactDetails' onClick={()=>{ setContactInfo(null)}}>
+    <a href="tel:+16035665610">Call <MdCall/></a>
+    <a href="sms://+16035665610">Text <MdSms/></a> 
+    <a href="mailto: joann333carter@gmail.com">Email <MdEmail/></a> 
+   </div>
+ )
+ const handleContactInfo = ()=>{
+  
+  if (details ){
+    setContactInfo(contactInfo)
+    setDetails(null)
+  }else {
+    setDetails(contactMe)
+    setContactInfo(null)
+  }
+}
+
   return (
     <div className="mainPage">
     
       <Menu/> 
+
       <div className={`card  ${available}`} onClick={()=>{ 
       handleAppStore()
-           }}>
-             <MdGetApp/>
-             <p>Download My App</p>
-             
-           </div>
-     <div className='card contact'onClick={()=>{
-      handleContactInfo() 
-     }}>
-        <MdEmail/>
-       <p>Contact Me</p>
-     </div>
-     {/* <a href="tel:+16035665610">Call <MdCall/></a>
-                 <a href="sms://+16035665610">Text <MdSms/></a> 
-                 <a href="mailto: joann333carter@gmail.com">Email <MdEmail/></a> */}
+        }}>
+          <MdGetApp/>
+         <p>Download My App</p>     
+      </div>
+      <div className='card contact'onClick={()=>{
+    handleContactInfo() 
+   }}>
+      {details}
+     
+ 
+   </div>
+   {contact}
+
       {/* <img src={star} className='star' alt='star' />
       <img src={star} className='star2' alt='star' />
       <img src={star} className='star3' alt='star' />
