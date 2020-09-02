31d8ecd77d27194ef356231c0cb870bc1b40fcb1
lioness3 <jcnailtech3@yahoo.com>
2020-09-01 23:39:01 -0400

    change main page layout

--- src/components/ForHire.js
+++ src/components/ForHire.js
@@ -9,17 +9,12 @@ function ForHire(props) {
   if (props.show){
     return (
      <div className='moreDetails'>
-
-
         <p className='lineB'>Problem Solver</p>
         <p className='lineA'>Motivated</p>
         <p className='creative'>Creative</p>
-   
-       
-        
         <a href = {Pdf} target = "_blank" rel="noopener noreferrer" className=''> <p>Download my Resume for more details</p>  </a>
     
-    </div>
+     </div>
       );
   }else{
       return (
